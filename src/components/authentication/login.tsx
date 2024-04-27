05e544bb2c74af6ec6d44f9ec466794f5cebe649
luisxsssx <cd210902@gmail.com>
2024-04-26 19:23:40 -0700

    Modification of components

--- src/components/authentication/login.tsx
+++ src/components/authentication/login.tsx
@@ -1,77 +1,51 @@
 import NavBar from "../navbar";
-import logoNote from "../../../dist/logotwo.png";
 import "../../styles.css";
 
 export default function Login() {
   return (
     <NavBar>
-      <div className="container-lg justify-content">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-sm-6">
-            <div className="d-flex flex-row ps-4 pt-4">
-              <i
-                className="fas fa-crow fa-3x me-3"
-                style={{ color: "#709085" }}
-              ></i>
-            </div>
+      <div className="container-fluid">
+        <div className="row justify-content-center align-items-center vh-50">
+          <div className="col-12">
+            <div
+              className="card text-white my-4 mx-auto auth"
+              style={{ maxWidth: "450px", borderRadius: "1rem" }}
+            >
+              <div className="card-body p-5 d-flex flex-column align-items-center w-100">
+                <h2 className="fw-bold mb-2 text-uppercase p-2">Login</h2>
 
-            <div className="d-flex flex-column justify-content-center h-custom-2 w-75 pt-2">
-              <h3
-                className="fw-normal mb-3 ps-5 pb-3 text-center"
-                style={{ letterSpacing: "1px" }}
-              >
-                Login
-              </h3>
+                <div className="mb-4 mx-5 w-100">
+                  <label className="form-label text-white">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="formControlEmail"
+                    placeholder="name@example.com"
+                  />
+                </div>
 
-              <div className="mb-4 mx-5 w-100">
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="email"
-                  placeholder="Email address"
-                />
+                <div className="mb-4 mx-5 w-100">
+                  <label className="form-label text-white">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="formControlPassword"
+                  />
+                </div>
+
+                <button
+                  type="button"
+                  className="btn btn-outline-light mx-2 px-5 mb-4"
+                  style={{ borderRadius: "2rem" }}
+                >
+                  Login
+                </button>
               </div>
-
-              <div className="mb-4 mx-5 w-100">
-                <input
-                  type="password"
-                  className="form-control form-control-lg"
-                  id="password"
-                  placeholder="Password"
-                />
-              </div>
-
-              <button
-                className="btn btn-info mb-4 px-5 mx-5 w-100"
-                type="button"
-              >
-                Login
-              </button>
-
-              <p className="small mb-5 pb-lg-3 ms-5 text-center">
-                <a className="text-muted" href="#!">
-                  Forgot password?
-                </a>
-              </p>
-              <p className="ms-5 text-center">
-                Don't have an account?{" "}
-                <a href="#!" className="link-info">
-                  Register here
-                </a>
-              </p>
             </div>
-          </div>
-
-          <div className="col-sm-5 d-none d-sm-block px-0">
-            <img
-              src={logoNote}
-              alt="Login image"
-              className="w-100"
-              style={{ objectFit: "cover", objectPosition: "left" }}
-            />
           </div>
         </div>
       </div>
     </NavBar>
   );
+
 }
