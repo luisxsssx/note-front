3fe6b489edc057aa637da38e9b48a73c390da924
luisxsssx <cd210902@gmail.com>
2024-05-13 16:22:56 -0700

    Changes in the README

--- src/components/pages/HomPage.tsx
+++ src/components/pages/HomPage.tsx
@@ -1,13 +1,13 @@
 import HomeNavbar from "../layout/HomeNavbar";
 
-export default function HomePage(){
-    return(
-        <>
-        <HomeNavbar>
-            <div className="container-lg">
-                <h2>dkeded</h2>
-            </div>
-        </HomeNavbar>
-        </>
-    )
+export default function HomePage() {
+  return (
+    <>
+      <HomeNavbar>
+        <div className="container-lg">
+          NOTAS
+        </div>
+      </HomeNavbar>
+    </>
+  );
 }
